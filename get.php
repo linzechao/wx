0b65fd2fb101e633923f32ecfd938f07b3e41ce9
MR.Super <linzechao44@163.com>
2017-06-14 18:15:36 +0800

    完善

--- get.php
+++ get.php
@@ -3,31 +3,24 @@ $appId = 'wxd863176f873f6e90';
 $appSecret = '2af86337b61b664090e35214c82c6df9';
 $noncestr = 'Wm3WZYTPz0wzccnW';
 $timestamp = time();
-$url = 'http://wx.mrsuper.top/index.html';
+$url = 'http://wx.mrsuper.top/home.php';
 
 
 // 获取Token
-/*
 $getTokenURL = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appId.'&secret='.$appSecret;
 $tokenResponse = file_get_contents($getTokenURL);
 $token = json_decode($tokenResponse, true)['access_token'];
-*/
-$token = 'lGtuDNpGAMZQnFC5VqTCdA_KdO_esxhM1wusBCP7Z2yoq0ZPjbmRU-5diReIP1NNBTyESwjx8N7tJMIAqrdkY-NyONDyABTB_8zzKEsuZ_UNNFeAIAGGY';
 
 
 // 获取JS API Ticket
-/*
 $getJSapiTicket = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='.$token.'&type=jsapi';
 $ticketResponse = file_get_contents($getJSapiTicket);
 $jsapi_ticket = json_decode($ticketResponse, true)['ticket'];
-*/
-$jsapi_ticket = 'sM4AOVdWfPE4DxkXGEs8VObj4YPT20WnBsxcp-gWQj9JmMf3285BcrXnu1BdEx3Nsg11oTF--DR1JdAwF9lAyw';
 
 
 // 拼接签名
 $fit = 'jsapi_ticket='.$jsapi_ticket.'&noncestr='.$noncestr.'&timestamp='.$timestamp.'&url='.$url;
 $signature = sha1($fit);
-
 
 
 echo json_encode(array(
--- home.php
+++ home.php
@@ -2,10 +2,69 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <title>欢迎来到MR联盟</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="renderer" content="webkit">
+    <link href="https://cdn.bootcss.com/element-ui/1.3.5/theme-default/index.css" rel="stylesheet">
+    <style>
+        body {
+            margin: 0;
+        }
+
+        header {
+            height: 40px;
+            line-height: 40px;
+            text-align: center;
+            color: white;
+            background-color: black;
+        }
+
+        h1 {
+            margin-top: 0;
+            margin-bottom: 0;
+            font-size: 14px;
+        }
+
+        .imgSrc {
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
-    <script src="http://cdn.bootcss.com/zepto/1.2.0/zepto.min.js"></script>
+    <section id="app">
+        <header>
+            <h1>欢迎来到MR联盟</h1>
+        </header>
+
+        <el-row :gutter="20">
+            <el-col :span="6"><div class="grid-content bg-purple">
+                <el-button type="success" @click="chooseImage()">选择图片</el-button>
+            </div></el-col>
+
+            <el-col :span="6"><div class="grid-content bg-purple">
+                <el-button type="warning" @click="previewImage()">预览图片</el-button>
+            </div></el-col>
+
+            <el-col :span="6"><div class="grid-content bg-purple">
+                <el-button type="danger" @click="startRecord()">开始录音</el-button>
+            </div></el-col>
+
+            <el-col :span="6"><div class="grid-content bg-purple">
+                <el-button type="info" @click="stopRecord()">停止录音</el-button>
+            </div></el-col>
+
+            <el-col :span="6"><div class="grid-content bg-purple">
+                <el-button type="success" @click="playVoice()">播放录音</el-button>
+            </div></el-col>
+
+        </el-row>
+
+        <img class="test-img" :src="imgSrc">
+    </section>
+
+    <script src="https://cdn.bootcss.com/vue/2.3.3/vue.min.js"></script>
+    <script src="https://cdn.bootcss.com/element-ui/1.3.5/index.js"></script>
     <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
     <?php
         $appId = 'wxd863176f873f6e90';
@@ -13,36 +72,113 @@
         $noncestr = 'Wm3WZYTPz0wzccnW';
         $timestamp = time();
         $url = 'http://wx.mrsuper.top/home.php';
-        $token = 'lGtuDNpGAMZQnFC5VqTCdA_KdO_esxhM1wusBCP7Z2yoq0ZPjbmRU-5diReIP1NNBTyESwjx8N7tJMIAqrdkY-NyONDyABTB_8zzKEsuZ_UNNFeAIAGGY';
-        $jsapi_ticket = 'sM4AOVdWfPE4DxkXGEs8VObj4YPT20WnBsxcp-gWQj9JmMf3285BcrXnu1BdEx3Nsg11oTF--DR1JdAwF9lAyw';
-        // 拼接签名
+        $token = 'WjedJvrLg9mUys3WUyp_8V92HGoCPqqBAwvtwJQYhF7eMswhirP4bl-uTpU1IysfzWtuFzLtfPANGWNbkrMsk_hOLED0Py_iyDGEpiifRqqTl0u4YD-Vzp3DOoDpnTjiATCdABAMFO';
+        $jsapi_ticket = 'sM4AOVdWfPE4DxkXGEs8VObj4YPT20WnBsxcp-gWQj-HaGGGWLOihVtpPUqI2SEzbvaIVXeR_OevdRJMHhAyLQ';
         $fit = 'jsapi_ticket='.$jsapi_ticket.'&noncestr='.$noncestr.'&timestamp='.$timestamp.'&url='.$url;
         $signature = sha1($fit);
     ?>
 
     <script>
-        (function() {
-            initWX({
+        new Vue({
+            beforeCreate: function() {
+                initWX();
+            },
+            el: '#app',
+            data: {
+                imgSrc: '',
+                record: ''
+            },
+            methods: {
+                chooseImage: function() {
+                    var that = this;
+                    wx.chooseImage({
+                        count: 4, // 默认9
+
+                        // 可以指定是原图还是压缩图，默认二者都有
+                        sizeType: ['original', 'compressed'],
+
+                        // 可以指定来源是相册还是相机，默认二者都有
+                        sourceType: ['album', 'camera'],
+                        success: function (res) {
+                            // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                            var localIds = res.localIds;
+                            that.imgSrc = localIds;
+                        }
+                    }); 
+                },
+
+                previewImage: function() {
+                    wx.previewImage({
+                        current: 'http://pic.ffpic.com/files/2014/0513/0512hbstazsjbz1.jpg',
+                        urls: [
+                            'http://img.r1.market.hiapk.com/data/upload/software/2011/3/4/4a0502672bdc4e7d8ce845dcd1cfd7ef/e331efe1e611410bbce348a0f6b90af1.jpeg',
+                            'http://img.r1.market.hiapk.com/data/upload/2011/10_21/56beb4909d6c4572a1d16a222d88bdd2.jpeg',
+                            'http://pic.ffpic.com/files/2014/0513/0512hbstazsjbz1.jpg',
+                            'http://img.r1.market.hiapk.com/data/upload/2014/01_14/23/201401142353362078.jpg'
+                        ]
+                    });
+                },
+
+                startRecord: function() {
+                    wx.startRecord();
+                },
+
+                stopRecord: function(res) {
+                    var that = this;
+                    wx.stopRecord({
+                        success: function(res) {
+                            that.record = res.localId; 
+                        }
+                    });
+                },
+
+                playVoice: function() {
+                    wx.playVoice({localId: this.record});
+                }
+            }
+        });
+
+        function initWX() {
+            var isFial;
+
+            wx.config({
                 appId: '<?= $appId ?>',
                 timestamp: <?= $timestamp ?>,
                 nonceStr: '<?= $noncestr ?>',
-                signature: '<?= $signature ?>'
-            });
-        }());
+                signature: '<?= $signature ?>',
+                jsApiList: [
+                    'hideOptionMenu',
+                    'showMenuItems',
+                    'chooseImage',
+                    'previewImage',
 
-        function initWX(config) {
-            wx.config({
-                debug: true,
-                appId: config.appId,
-                timestamp: config.timestamp,
-                nonceStr: config.nonceStr,
-                signature: config.signature,
-                jsApiList: ['hideAllNonBaseMenuItem']
+                    'startRecord',
+                    'stopRecord',
+                    'playVoice'
+                ]
             }); 
 
+            // 失败
+            wx.error(function(res) {
+                alert('微信配置失败：' + res);
+                isFial = true;
+            });
+
+            // 加载好运行(不论成功失败)
             wx.ready(function() {
-                // 隐藏所有非基础按钮接口
-                wx.hideAllNonBaseMenuItem();
+                // 隐藏右上角菜单
+                wx.hideOptionMenu();
+
+                if (!isFial) {
+                    wx.showMenuItems({
+                        menuList: [
+                            'menuItem:share:appMessage',
+                            'menuItem:share:timeline',
+                            'menuItem:share:qq',
+                            'menuItem:share:QZone'
+                        ]
+                    });
+                }
             });
         }
     </script>
